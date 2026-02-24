d1ba340732ad44ad51c56fa60d4e0befb4a311f4
kelvinnzyoki <kelvinnzyokimaitha@gmail.com>
2026-02-24 22:26:29 +0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import 'dotenv/config';
-import express, { Application, Request, Response } from 'express';
+import { rateLimit } from 'express-rate-limit';
 import cors from 'cors';
 import {
   contentSecurityPolicy,
