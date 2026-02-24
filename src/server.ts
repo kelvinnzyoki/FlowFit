c219feca5fa05480e195fd4b596f85ea236c8172
kelvinnzyoki <kelvinnzyokimaitha@gmail.com>
2026-02-24 15:28:09 +0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
-import helmet from 'helmet';
+import * as helmet from 'helmet';
 import compression from 'compression';
 import morgan from 'morgan';
 import logger, { morganStream } from './utils/logger.js';
@@ -22,7 +22,7 @@ const app: Application = express();
 // ============================================
 
 app.use(
-  helmet({
+  helmet.default({
     contentSecurityPolicy: {
       directives: {
         defaultSrc: ["'self'"],
