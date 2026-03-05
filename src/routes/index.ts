c2c7d77cf4e86a457d952723e6bb3343c4c7213e
kelvinnzyoki <kelvinnzyokimaitha@gmail.com>
2026-03-06 02:29:16 +0300

    Update index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -45,7 +45,7 @@ import programRoutes      from './program.routes.js';
 import progressRoutes     from './progress.routes.js';
 import userRoutes         from './user.routes.js';
 import subscriptionRoutes from './subscription.routes.js';
-import seedRoutes from './seed.route.js';      // ← add this
+import seedRoute from './seed.route.js';      // ← add this
 
      
 const router = Router();
@@ -73,6 +73,6 @@ router.use('/users',         userRoutes);
 router.use('/subscriptions', subscriptionRoutes);
 
 
-router.use('/seed', seedRoutes);   
+router.use('/seed', seedRoute);   
 
 export default router;
