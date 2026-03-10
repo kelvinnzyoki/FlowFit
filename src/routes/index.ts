7ca3f0f0041eddf727b620bc2e5d565110e46042
kelvinnzyoki <kelvinnzyokimaitha@gmail.com>
2026-03-11 00:20:18 +0300

    Update index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -45,7 +45,7 @@ import programRoutes      from './program.routes.js';
 import progressRoutes     from './progress.routes.js';
 import userRoutes         from './user.routes.js';
 import subscriptionRoutes from './subscription.routes.js';
-import seedRouter from './src/routes/seed.routes.js';
+import seedRouter from './seed.routes.js';
 
 
      
