0e3607535a444acbdcfae80a8c0c33eeeaf5f2a7
kelvinnzyoki <kelvinnzyokimaitha@gmail.com>
2026-03-10 20:48:28 +0300

    Update plans.seed.ts

--- prisma/plans.seed.ts
+++ prisma/plans.seed.ts
@@ -6,7 +6,7 @@
  */
 
 import { PrismaClient } from '@prisma/client';
-import { PLAN_SEEDS } from './plans.config.js';
+import { PLAN_SEEDS } from './src/config/plans.config.js';
 
 const prisma = new PrismaClient();
 
