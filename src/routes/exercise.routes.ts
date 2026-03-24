2adb3066a8ea0c3a2d10acdf85d2a4e208cc1cee
kelvinnzyoki <kelvinnzyokimaitha@gmail.com>
2026-03-24 21:41:09 +0300

    Update exercise.routes.ts

--- src/routes/exercise.routes.ts
+++ src/routes/exercise.routes.ts
@@ -1,5 +1,5 @@
-import express from "express";
-import { fetchExercises } from "./exercise.service";
+import { Request, Response, NextFunction } from 'express';
+import { fetchExercises } from "./exercise.service.js";
 
 const router = express.Router();
 
