9cc36916d0d013000dd9120c7abde4d0cd323328
kelvinnzyoki <kelvinnzyokimaitha@gmail.com>
2026-04-16 03:20:25 +0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -61,8 +61,7 @@ app.use(
 
 app.use(
   cors({
-    origin:         ["process.env.CORS_ORIGIN || '*'",
-                     "https://aicoach-snowy.vercel.app"],
+    origin:         process.env.CORS_ORIGIN || '*',
     credentials:    true,
     methods:        ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
